6a8ae6ebceed7325ee01960af9f2142cf2acba77
Sana Ahsan <sanaahsanmallick@gmail.com>
2025-01-04 23:01:38 +0530

    Added

---
+++ Basic_Programs/Sum_of_square_of_digits.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Sum_of_square_of_digits {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num= input.nextInt();
+        int sum = 0;
+        while(num>0){
+
+            int temp=num%10;
+            sum= sum+(temp*temp);
+
+            num=num/10;
+        
+        }
+        System.out.println(sum);
+    }
+}
