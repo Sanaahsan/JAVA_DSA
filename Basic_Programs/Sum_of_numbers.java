08f660593fb82eb4d974ab4e4efabdaa7cea4609
Sana Ahsan <sanaahsanmallick@gmail.com>
2025-01-04 23:03:21 +0530

    Added

---
+++ Basic_Programs/Sum_of_numbers.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Sum_of_numbers{
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        int a =input.nextInt();
+        int b = input.nextInt();
+        int Sum= a+b;
+        System.out.println(Sum);
+        
+
+    }
+}
